e0008cdc3ebdcce388f5d9369b642013b99e9426
Parth Khanna <parth.khanna@mail.mcgill.ca>
2017-09-07 09:48:44 -0400

    reactive contact form

--- conFusion/src/app/app.module.ts
+++ conFusion/src/app/app.module.ts
@@ -6,6 +6,7 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { MaterialModule } from '@angular/material'; 
 import { FlexLayoutModule } from '@angular/flex-layout';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import 'hammerjs';
 
@@ -46,7 +47,8 @@ import { LoginComponent } from './login/login.component'
     HttpModule,
     MaterialModule,
     FlexLayoutModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [ DishService, PromotionService, LeaderService],
   entryComponents: [
