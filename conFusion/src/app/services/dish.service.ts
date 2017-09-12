0c824a8cd2d9f2ac21da09c51a7249df36248316
Parth Khanna <parth.khanna@mail.mcgill.ca>
2017-09-12 19:30:15 -0400

    route menu items

--- conFusion/src/app/services/dish.service.ts
+++ conFusion/src/app/services/dish.service.ts
@@ -21,4 +21,8 @@ export class DishService {
   getFeaturedDish(): Observable<Dish> {
     return Observable.of(DISHES.filter((dish) => dish.featured)[0]).delay(2000);
   }
+
+  getDishIds(): Observable<number[]>{
+    return Observable.of(DISHES.map(dish => dish.id)).delay(2000);
+  }
 }
