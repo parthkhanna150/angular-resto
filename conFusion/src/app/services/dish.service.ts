c9cdabe478055c61e64b742c46797239a3f82a3a
Parth Khanna <parth.khanna@mail.mcgill.ca>
2017-09-12 11:27:13 -0400

    observable .of error handled

--- conFusion/src/app/services/dish.service.ts
+++ conFusion/src/app/services/dish.service.ts
@@ -3,6 +3,7 @@ import { Dish } from '../shared/dish';
 import { DISHES } from '../shared/dishes';
 import { Observable } from 'rxjs/Observable';
 
+import 'rxjs/add/observable/of';
 import 'rxjs/add/operator/delay';
 
 @Injectable()
